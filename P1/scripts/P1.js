d1634c61cfc021666e758f5570278e9e42d7bea4
RougeGod <hurricanesman@gmx.com>
2023-03-06 19:57:01 -0400

    added array of image names

---
+++ P1/scripts/P1.js
@@ -0,0 +1,5 @@
+//The images representing miqmaq words
+const IMAGE_NAMES = ["aqq.jpg", "eliey.jpg",   "kesalk.jpg", 
+                     "kil.jpg", "ltu.jpg",     "mijisi.jpg", 
+                     "nin.jpg", "teluisi.jpg", "wiktm.jpg"] 
+const IMAGE_PATH = "./"
