70b2d8dd357b58de4c04b40b3a1fdec65a176044
BulmerBoy <127059194+BulmerBoy@users.noreply.github.com>
2023-03-10 16:45:57 -0400

    Adding some drag and drop API

--- P1/scripts/P1.js
+++ P1/scripts/P1.js
@@ -5,7 +5,7 @@ const IMAGE_NAMES = ["aqq.jpg", "eliey.jpg",   "kesalk.jpg",
 const IMAGE_PATH = "./"
 
 //global variables
-var correctAnswer,
+let correctAnswer,
     decision;
 
 
@@ -42,3 +42,64 @@ function checkAnswer(decision){
 function RandomInt(max){
     return Math.floor(math.random() * max);
 }
+
+
+/**
+ * This function stores the id of the element being dragged 
+ * in a storage area under the key "text". Target is the
+ * element being dragged. 
+ * 
+ * @param event the event object being loaded for dragging
+ * 
+ * Author: Caleb Bulmer
+ */
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+/**
+ * This functions suspends default behaviour for a 
+ * potential new position for the dragged element. Target
+ * is the potential element where dragged element could be 
+ * dropped.
+ * 
+ * @param event the event object being loaded for allowDrop
+ * @param image The passed in image to be hidden.
+ * 
+ * Author: Caleb Bulmer
+ */
+function allowDrop(event, image) {
+    event.preventDefault();
+    // console logs are to see what is happening while program runs.
+    console.log(event.target.id);
+    console.log("imageNum=" + imageNum);
+    /* The idea here being that we could show every image before hiding
+    a particular image. Can be expanded on once the grid seems to be
+    good. */
+    $(aqq.jpg).show();
+    $(eliey.jpg).show();
+    $(kesalk.jpg).show();
+    $(kil.jpg).show();
+    $(ltu.jpg).show();
+    $(mijisi.jpg).show();
+    $(nin.jpg).show();
+    $(teluisi.jpg).show();
+    $(wiktm.jpg).show();
+    $(image).hide();
+}
+
+/**
+ * This function is what will happen when the dragged
+ * element is decided to be dropped in the position. 
+ * 
+ * @param event the event object being loaded for drop
+ * 
+ * Author: Caleb Bulmer
+ */
+function drop(event) {
+    event.preventDefault();
+  
+    // contains the id of the element that was being dragged
+    let data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
+  }
